f1e2cb0d700efda929371ab95dea6aa58dc40377
Greg Jenner <jennergreg733@gmail.com>
2022-12-10 11:26:04 -0500

    fix stylesheet ref in index.js

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './src/styles/styles.scss';
+import './stylesheets/styles.scss';
 import App from './src/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
