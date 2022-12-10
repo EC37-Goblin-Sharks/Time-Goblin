9bb69715bc0d2addb9f0c7dff09afdf14b0bd84b
nathanycho <nathan.y.cho@gmail.com>
2022-12-10 16:53:13 -0500

    this commit will add the Timer functionality

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
+import Timer from './Timer';
+
 const Home = (props) => {
   // ==========================
   // Set State
   // ==========================
   // get request for user name, pass into useState below
-  const [user, setUser] = useState();
-  const [points, setPoints] = useState();
+  const [user, setUser] = useState('');
+  const [points, setPoints] = useState(0);
+
 
   // ==========================
   // Functions
@@ -15,6 +18,7 @@ const Home = (props) => {
   const addPoints = () => {
     // setPoints(); // increment points here
   }
+
 
   // ==========================
   // Render this
@@ -28,8 +32,7 @@ const Home = (props) => {
       </div>
       <div className='homeBody'>
         {/* add decorations */}
-        <div className='timer'>00:10</div>
-        <button className='timerBtn'>Start Timer!</button>
+        <Timer />
         {/* add decorations */}
       </div>
     </div>
---
+++ client/components/Timer.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = () => {
+
+  // Credits: https://dev.to/emmaadesile/build-a-timer-using-react-hooks-3he2 
+  const [second, setSecond] = useState('00');
+  const [minute, setMinute] = useState('25');
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(1499); // 25 minutes * 60 seconds - 1 second for the setInterval delay
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60);
+
+        const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}` : secondCounter;
+        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
+
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+
+        setCounter(counter => counter - 1);
+      }, 1000)
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter]);
+
+  function resetTimer() {
+    setIsActive(false);
+    setCounter(1499);
+    setMinute('25');
+    setSecond('00');
+  }
+
+  return (
+    <div className="container">
+      <div className="time">
+        <span className="minute">{minute}</span>
+        <span>:</span>
+        <span className="second">{second}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={() => setIsActive(!isActive)} className="start">
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button onClick={resetTimer} className="reset">Reset</button>
+      </div>
+    </div>
+  )
+}
+
+export default Timer;
