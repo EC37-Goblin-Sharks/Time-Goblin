6edf573af21278f7c22f601198610ece55c9e5f8
nathanycho <nathan.y.cho@gmail.com>
2022-12-10 14:07:33 -0500

    this commit will add components and create a basic sign up post form page

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-function App() {
+import Signup from './components/Signup';
+
+const App = (props) => {
   return (
-    <div id='appDiv'>
+    <div className="router">
       hello world
       {/* content seen on every page */}
-      <div id='content'>
+      <div className="routerMain" id='content'>
         <Routes>
           {/* different path inside each rout, component inside- loggin, sign up, home */}
-          <Route path='/' />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </div>
     </div>
---
+++ client/components/Signup.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const Signup = (props) => {
+  return (
+    <div className="signupPage">
+      <div className="signupBox">
+        <form className="signupForm" method="POST" action="/signup">
+          <label htmlFor="username">Username: </label>
+          <input className="formInfo" id="username" name="username" type="text"></input>
+          <label htmlFor="password">Password: </label>
+          <input className="formInfo" id="password" name="password" type="password"></input>
+          <label htmlFor="firstName">First Name: </label>
+          <input className="formInfo" id="firstName" name="firstName" type="text"></input>
+          <label htmlFor="lastName">Last Name: </label>
+          <input className="formInfo" id="lastName" name="lastName" type="text"></input>
+          <input className="signupButton" type="submit" value="Sign Up"></input>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Signup;
