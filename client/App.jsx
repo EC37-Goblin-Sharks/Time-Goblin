11df0bfe861d5778dde10bdc4c39b14ba6853d4c
nathanycho <nathan.y.cho@gmail.com>
2022-12-10 14:36:42 -0500

    oops. this commit will import the login page

--- client/App.jsx
+++ client/App.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 const App = (props) => {
   return (
     <div className="router">
-      hello world
+      time goblin!
       {/* content seen on every page */}
       <div className="routerMain" id='content'>
         <Routes>
