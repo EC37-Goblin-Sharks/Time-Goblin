162e3d2a7cc8edba290a67dc9234f614bc19c426
Greg Jenner <jennergreg733@gmail.com>
2022-12-12 11:39:23 -0500

    add get request for user name and points

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { response } from 'express';
 import React, { useState, useEffect } from 'react';
 
 import Timer from './Timer';
@@ -10,33 +12,59 @@ const Home = (props) => {
   const [user, setUser] = useState('');
   const [points, setPoints] = useState(0);
 
-
   // ==========================
   // Functions
   // ==========================
 
-  const addPoints = () => {
+  const addPointsDb = () => {
     // setPoints(); // increment points here
-  }
+    axios
+      .post('/points', {
+        points: points,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  const getUserInfo = () => {
+    axios
+      .get('http://localhost.3000/home', { withCredentials: true })
+      .then((res) => {
+        setUser(res.data.firstName);
+        setPoints(res.data.points);
+      });
+  };
+
+  useEffect(() => {
+    // get request for user and points
+    getUserInfo();
+  });
 
   // ==========================
   // Render this
   // ==========================
   return (
-    <div className="homePage">
+    <div className='homePage'>
       <div className='homeHeader'>
-        <div className="greeting">Hello, Goblin</div>
-        <div className="points">You have 0 pts</div>
+        <div className='greeting'>Hello, Goblin</div>
+        <div className='points'>You have 0 pts</div>
         {/* add store div */}
       </div>
       <div className='homeBody'>
         {/* add decorations */}
-        <Timer />
+        <Timer
+        // points={points}
+        // setPoints={setPoints}
+        // addPointsDb={addPointsDb}
+        />
         {/* add decorations */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
